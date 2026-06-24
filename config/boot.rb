a54b5131d03c5710034444a70a0d0f5a5aef9dd5
MahmudulHasanRiaz <mahmudriaz.bd@gmail.com>
2026-06-24 13:35:56 +0600

    feat: initialize dotenv in boot process to support early configuration loading

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+# Load .env early so lib/keygen.rb can read KEYGEN_HOST and other vars
+require "dotenv/load"
